790039bd6beabc9fe352495c82567dc85ca66c51
Михаил Малинко <114336090+mikhail-malinko@users.noreply.github.com>
2023-03-28 20:04:53 +0300

    Update CardOrderPositiveTest.java

--- src/test/java/ru/netology/cardOrder/CardOrderPositiveTest.java
+++ src/test/java/ru/netology/cardOrder/CardOrderPositiveTest.java
@@ -26,7 +26,7 @@ public class CardOrderPositiveTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-//         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
